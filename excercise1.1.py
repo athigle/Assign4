e9e0d29dd3f9d4bbc7d1fa29215b184ef5e41a50
athigle <athigle@smu.edu>
2019-10-04 20:53:12 -0500

    first 2 files added

---
+++ excercise1.1.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+print("creating aircraft dictionary:--")
+aircraft=dict()
+aircraft["x"]=21
+aircraft["y"]=41
+print("aircraft dictionary x: {}".format(aircraft["x"]))
+print("aircraft dictionary y: {}".format(aircraft["y"]))
